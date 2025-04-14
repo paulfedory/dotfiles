25faa5d9819d9d1d816367a0ce35254ae2279ba5
Paul Fedory <paul.fedory@gmail.com>
2025-04-14 10:10:58 -0400

    Add golang versions

--- vim/lua/plugins/lsp.lua
+++ vim/lua/plugins/lsp.lua
@@ -258,6 +258,7 @@ return {
       local ensure_installed = vim.tbl_keys(servers or {})
       vim.list_extend(ensure_installed, {
         'stylua', -- Used to format Lua code
+        { 'gopls', version = "v0.18.1" },
       })
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
