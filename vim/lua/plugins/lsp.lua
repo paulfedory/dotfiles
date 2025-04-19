02781c8513996ffc3418f2b46fce8c332253407b
Paul Fedory <paul.fedory@gmail.com>
2025-04-18 23:48:26 -0400

    Stop jumping in LSP

--- vim/lua/plugins/lsp.lua
+++ vim/lua/plugins/lsp.lua
@@ -82,16 +82,16 @@ return {
           map('gra', vim.lsp.buf.code_action, '[G]oto Code [A]ction', { 'n', 'x' })
 
           -- Find references for the word under your cursor.
-          map('grr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
+          map('grr', function() require('telescope.builtin').lsp_references({jump_type = "never"}) end, '[G]oto [R]eferences')
 
           -- Jump to the implementation of the word under your cursor.
           --  Useful when your language has ways of declaring types without an actual implementation.
-          map('gri', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
+          map('gri', function() require('telescope.builtin').lsp_implementations({jump_type = "never"}) end, '[G]oto [I]mplementation')
 
           -- Jump to the definition of the word under your cursor.
           --  This is where a variable was first declared, or where a function is defined, etc.
           --  To jump back, press <C-t>.
-          map('grd', require('telescope.builtin').lsp_definitions, '[G]oto [D]efinition')
+          map('grd', function() require('telescope.builtin').lsp_definitions({jump_type = "never"}) end, '[G]oto [D]efinition')
 
           -- WARN: This is not Goto Definition, this is Goto Declaration.
           --  For example, in C this would take you to the header.
